59310b42deeb818640e21c09847bada0f36802a5
Fenris04 <marc.maehling@gmail.com>
2026-03-14 16:35:54 +0100

    Calculator gebaut und bei jeden getestet (drin gelassen)

--- js/script.js
+++ js/script.js
@@ -22,3 +22,36 @@ function sendMail(event){
         console.log(error);
     });
   }
+
+  // Ingredient Calculator 
+  const ingredientTable = document.getElementById("ingredientTable");
+
+  let ingredientList;
+
+  let error = "Bitte geben Sie eine gültige Anzahl von 1 bis 20 ein.";
+
+  // calculation for a single Portion
+  if (ingredientTable) {
+    ingredientList = ingredientTable.innerHTML;
+  }
+
+  // calculator for multiple Portions
+  if (ingredientTable) {
+    function calculatePortion(event) {
+      event.preventDefault();
+      let value = document.getElementById("portions").value;
+      document.getElementById("error").innerHTML = "";
+
+      if(value < 1 || value > 20) {
+        value = 1; 
+        document.getElementById("portions").value = 1;
+        document.getElementById("error").innerHTML = error;
+      }
+      
+      let newIngredientList = ingredientList.replaceAll(/\d+/g, (match) => {
+        return parseInt(match) * value;
+      });
+
+      document.getElementById("ingredientTable").innerHTML = newIngredientList;
+    }
+  }
